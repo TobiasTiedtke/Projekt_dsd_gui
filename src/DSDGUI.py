c0f6a4482703d51c03995b4d61451a8951d641d6
TobiasTiedtke <56926744+TobiasTiedtke@users.noreply.github.com>
2019-11-06 17:31:52 +0100

    Update DSDGUI.py
    Adding in Drag Function

--- src/DSDGUI.py
+++ src/DSDGUI.py
@@ -14,6 +14,18 @@ class Ui(QtWidgets.QDialog):
 	SaveButton = self.findChild(QtWidgets.QPushButton, 'SaveButton')
 	SaveButton.clicked.connect(SaveB.SaveButton)
         self.show()
+    #Function to enable drag of text
+    def dragEnterEvent(self, e):
+
+        if e.mimeData().hasFormat('text/plain'):
+            e.accept()
+        else:
+            e.ignore()
+    #Function to drop text
+    def dropEvent(self, e):
+
+        self.setText(e.mimeData().text())
+
     def SingleBrowse(self):
 	#Open directory
         filePath = QtWidgets.QFileDialog.getOpenFileNames(self, 'File Browser', "-/Desktop/",'')

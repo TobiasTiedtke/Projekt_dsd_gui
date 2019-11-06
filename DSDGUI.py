3db492c77e940d5d1a9e440c455f9aed61aa2571
TobiasTiedtke <56926744+TobiasTiedtke@users.noreply.github.com>
2019-11-06 14:42:02 +0100

    Add files via upload
    Buttons - Simply able to open the file explorer

---
+++ DSDGUI.py
@@ -0,0 +1,26 @@
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
+import os, sys
+
+class Ui(QtWidgets.QDialog):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('DSDGUI.ui', self)
+        DecisionFolderButton = self.findChild(QtWidgets.QPushButton, 'DecisionFolderButton')
+        DecisionFolderButton.clicked.connect(self.SingleBrowse)
+        ActionFolderButton = self.findChild(QtWidgets.QPushButton, 'ActionFolderButton')
+        ActionFolderButton.clicked.connect(self.SingleBrowse)
+        self.show()
+    def SingleBrowse(self):
+        filePath = QtWidgets.QFileDialog.getOpenFileNames(self, 'File Browser', "-/Desktop/",'*.txt.')
+        print('filePath', filePath, '\n')
+        fileHandle = open( filePath, 'r')
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    sys.exit(app.exec_())
+
+
+
+
+
